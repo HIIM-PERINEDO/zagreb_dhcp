d700fa57321feab0c1cb3bed4e03cfbc77f351a7
Finn Lennartsson <finn.lennartsson@gmail.com>
2020-12-04 10:33:19 +0100

    Added anatomical sequences tra T2w and cor T2w

--- bids/heudiconv_heuristics/zagreb_heuristic.py
+++ bids/heudiconv_heuristics/zagreb_heuristic.py
@@ -38,6 +38,8 @@ def infotodict(seqinfo):
     # ANATOMY
     t1w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_T1w')
     t2w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_T2w')
+    t2wcor = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-cor_T2w')
+    t2wtra = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-tra_T2w')
     flair = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_FLAIR')
     
     # DWI
@@ -60,7 +62,7 @@ def infotodict(seqinfo):
     rest_ap_sbref = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_dir-AP_sbref')
     rest_pa_sbref = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_dir-PA_sbref')
 
-    info = {t1w: [], t2w: [], flair: [], dwi_ap: [], rest_ap: [], rest_pa: [], fmap_se_ap: [], fmap_se_pa: [], dwi_ap_sbref: [], dwi_pa_sbref: [], rest_ap_sbref: [], rest_pa_sbref: []}
+    info = {t1w: [], t2w: [], t2wcor: [], t2wtra: [], flair: [], dwi_ap: [], rest_ap: [], rest_pa: [], fmap_se_ap: [], fmap_se_pa: [], dwi_ap_sbref: [], dwi_pa_sbref: [], rest_ap_sbref: [], rest_pa_sbref: []}
     #info = {t1w: [], t2w: [], dwi: [], dwi_sbref: [], rest: [], rest_sbref: [], fmap_se}
     last_run = len(seqinfo)
 
@@ -96,6 +98,12 @@ def infotodict(seqinfo):
         # 3D T2w
         if ('T2w_SPC' in s.protocol_name) and ('NORM' in s.image_type): # takes normalized images
             info[t2w] = [s.series_id] # assign if a single series meets criteria
+        # T2w Ax
+        if ('t2_qtse_tra' in s.protocol_name): 
+            info[t2wtra] = [s.series_id] # assign if a single series meets
+        # T2w Cor
+        if ('t2_qtse_cor' in s.protocol_name): 
+            info[t2wcor] = [s.series_id] # assign if a single series meets criteria
         # FLAIR
         if ('t2_space_dark-fluid_sag_iso' in s.protocol_name) and ('NORM' in s.image_type): # takes normalized images
             info[flair] = [s.series_id] # assign if a single series meets criteria
