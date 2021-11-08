a856d47926552b982e20725776808b88b610e5f6
Finn Lennartsson <finn.lennartsson@gmail.com>
2021-11-08 13:51:40 +0100

    changes

--- sMRI/neonatal-5TT_DrawEM.sh
+++ sMRI/neonatal-5TT_DrawEM.sh
@@ -5,7 +5,7 @@ usage()
 {
   base=$(basename "$0")
   echo "usage: $base subjectID sessionID age [options]
-Creates a 5TT equivalent from resulting anatomical DrawEM parcellation (all_labels parcelleation file)
+Creates a 5TT equivalent from resulting anatomical DrawEM parcellation (all_labels parcellation file)
 Arguments:
   sID				Subject ID (e.g. PMR001) 
   ssID                       	Session ID (e.g. MR2)
@@ -29,13 +29,13 @@ ssID=$2
 currdir=$PWD
 T2seg=derivatives/sMRI/sub-$sID/ses-$ssID/neonatal-segmentation/segmentations/sub-${sID}_ses-${ssID}_desc-preproc_T2w_all_labels.nii.gz
 mask=derivatives/sMRI/sub-$sID/ses-$ssID/neonatal-segmentation/segmentations/sub-${sID}_ses-${ssID}_desc-preproc_brain_mask.nii.gz
-datadir=derivatives/dMRI/sub-$sID/ses-$ssID
+datadir=derivatives/sMRI/sub-$sID/ses-$ssID/neonatal-segmentation
 threads=10
 atlas=ALBERT
 
 # check whether the different tools are set and load parameters
 codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-currdir=`pwd`
+currdir=$PWD
 
 shift; shift; shift
 while [ $# -gt 0 ]; do
@@ -53,14 +53,13 @@ while [ $# -gt 0 ]; do
 done
 
 echo "Neonatal segmentation using DrawEM
-Subject:    $sID 
-Session:    $ssID
-Age:        $age
-T2:         $T2 
-Mask:	    $mask 
-Directory:  $datadir 
-Threads:    $threads
-$BASH_SOURCE $command
+Subject:       $sID 
+Session:       $ssID
+Segmentation:  $T2seg 
+Mask:	       $mask 
+Directory:     $datadir 
+Threads:       $threads
+$BASH_SOURCE   $command
 ----------------------------"
 
 # Set up log
@@ -80,37 +79,39 @@ T2base=`basename $T2 .nii.gz`
 ################################################################
 ## 1. Create 5TT image
 
+if [ ! -d $datadir/5TT_$atlas ];then mkdir -p $datadir/5TT_$atlas; fi
+
 cd $datadir
 
 # Create subfolder 5TT to hold results
-if [ ! -d 5TT ];then mkdir 5TT; fi
+
 
 # Path to LUTs for conversion
 LUTdir=$codedir/../label_names/$atlas
 
-if [ ! -f 5TT/${T2base}_5TT.mif.gz ]; then
+if [ ! -f 5TT_$atlas/${T2base}_5TT.mif.gz ]; then
     # NOTE - for both all_labels_2_5TT.txt and all_labels_2_5TT_sgm_amyg_hipp.txt
     # 1 - Converts Intra-cranial-background to WM - This converts dWM properly (in tissue_labels => sGM) but there can be some extra-cerebral tissue that becomes included in WM! Check results!!
     # 2 - Converts cerebellum to subcortical-GM
     # NOTE - for all_labels_2_5TT_sgm_amyg_hipp.txt
     # 3 - Converts Amygdala and Hippocampi to subcortical-GM (change by using LUT all_labels_2_5TT.txt)
-    labelconvert segmentations/${T2base}_all_labels.nii.gz $LUTdir/all_labels.txt $LUTdir/all_labels_2_5TT_sgm_amyg_hipp.txt 5TT/${T2base}_5TTtmp.mif
+    labelconvert segmentations/${T2base}_all_labels.nii.gz $LUTdir/all_labels.txt $LUTdir/all_labels_2_5TT_sgm_amyg_hipp.txt 5TT_$atlas/${T2base}_5TTtmp.mif
     
     # Break up 5TTtmp in its individual components
-    mrcalc 5TT/${T2base}_5TTtmp.mif 1 -eq 5TT/${T2base}_5TTtmp_01.mif #cGM
-    mrcalc 5TT/${T2base}_5TTtmp.mif 2 -eq 5TT/${T2base}_5TTtmp_02.mif #sGM
-    mrcalc 5TT/${T2base}_5TTtmp.mif 3 -eq 5TT/${T2base}_5TTtmp_03.mif #WM
-    mrcalc 5TT/${T2base}_5TTtmp.mif 4 -eq 5TT/${T2base}_5TTtmp_04.mif #CSF
-    mrcalc T2/$T2base.nii.gz 0 -mul 5TT/${T2base}_5TTtmp_05.nii.gz #pathological tissue - create image with 0:s
+    mrcalc 5TT_$atlas/${T2base}_5TTtmp.mif 1 -eq 5TT_$atlas/${T2base}_5TTtmp_01.mif #cGM
+    mrcalc 5TT_$atlas/${T2base}_5TTtmp.mif 2 -eq 5TT_$atlas/${T2base}_5TTtmp_02.mif #sGM
+    mrcalc 5TT_$atlas/${T2base}_5TTtmp.mif 3 -eq 5TT_$atlas/${T2base}_5TTtmp_03.mif #WM
+    mrcalc 5TT_$atlas/${T2base}_5TTtmp.mif 4 -eq 5TT_$atlas/${T2base}_5TTtmp_04.mif #CSF
+    mrcalc T2/$T2base.nii.gz 0 -mul 5TT_$atlas/${T2base}_5TTtmp_05.nii.gz #pathological tissue - create image with 0:s
     # and put together in 4D 5TT-file
-    mrcat -axis 3  5TT/${T2base}_5TTtmp_0*.mif 5TT/${T2base}_5TT.mif.gz
+    mrcat -axis 3  5TT_$atlas/${T2base}_5TTtmp_0*.mif 5TT_$atlas/${T2base}_5TT.mif.gz
     # remove tmp-files
-    rm 5TT/*tmp*
+    rm 5TT_$atlas/*tmp*
     
     # Create some 5TT maps for visualization
-    if [ ! -f 5TT/${T2base}_5TTvis.mif.gz ];then
-	5tt2vis 5TT/${T2base}_5TT.mif.gz 5TT/${T2base}_5TTvis.mif.gz;
-	5tt2gmwmi 5TT/${T2base}_5TT.mif.gz 5TT/${T2base}_5TTgmwmi.mif.gz;
+    if [ ! -f 5TT_$atlas/${T2base}_5TTvis.mif.gz ];then
+	5tt2vis 5TT_$atlas/${T2base}_5TT.mif.gz 5TT_$atlas/${T2base}_5TTvis.mif.gz;
+	5tt2gmwmi 5TT_$atlas/${T2base}_5TT.mif.gz 5TT_$atlas/${T2base}_5TTgmwmi.mif.gz;
     fi
 fi
 
